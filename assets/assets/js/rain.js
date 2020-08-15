8a2e2770f1bf0619aa3c9f75a553417aa5d6f47e
Elusive Fox <elusivefox@elusivefox.net>
2020-08-15 10:23:31 +0000

    v 1.2

---
+++ assets/assets/js/rain.js
@@ -0,0 +1,51 @@
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = '01';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 20,
+    columns = canvas.width / fontSize;
+
+// Setting up the drops
+var drops = [];
+for (var i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+
+// Setting up the draw function
+function draw() {
+  ctx.fillStyle = 'rgba(16, 14, 23, .1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < drops.length; i++) {
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    // color
+    ctx.fillStyle = '#0bceaf';
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+      drops[i] = 0;
+    }
+  }
+}
+function update(e){
+  var x = e.clientX || e.touches[0].clientX
+  var y = e.clientY || e.touches[0].clientY
+
+  document.documentElement.style.setProperty('--cursorX', x + 'px')
+  document.documentElement.style.setProperty('--cursorY', y + 'px')
+}
+
+// Loop the animation
+setInterval(draw, 33);
+
+
+document.addEventListener('mousemove',update)
+document.addEventListener('touchmove',update)
